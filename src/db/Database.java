5919b52ce50c46f73dada179b27f596f8a89f026
Tom <tpepels@gmail.com>
2013-02-16 17:22:17 +0100

    database code and stuff

---
+++ src/db/Database.java
@@ -0,0 +1,43 @@
+package db;
+
+import java.sql.*;
+
+public class Database {
+
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection(String host, String username, String password) {
+		Connection connect = null;
+		String url = "jdbc:mysql://" + host + "/";
+		String dbName = "nano_tech";
+		try {
+			connect = (Connection) DriverManager.getConnection(url + dbName, username, password);
+			System.out.println("Connected to the database");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connect;
+	}
+
+	public static Statement createStreamingStatement(final Connection connection)
+			throws SQLException {
+		Statement statement = (Statement) connection.createStatement(ResultSet.TYPE_FORWARD_ONLY,
+				ResultSet.CONCUR_READ_ONLY);
+		// This makes sure the records are read 1 at a time, in stead of reading the whole result in memory
+		// (https://dev.mysql.com/doc/refman/5.0/en/connector-j-reference-implementation-notes.html)
+		statement.setFetchSize(Integer.MIN_VALUE);
+		return statement;
+	}
+
+	public static Statement createStatement(final Connection connection) throws SQLException {
+		Statement statement = (Statement) connection.createStatement(ResultSet.TYPE_FORWARD_ONLY,
+				ResultSet.CONCUR_READ_ONLY);
+		return statement;
+	}
+}
